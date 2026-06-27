03cdcca29740ea0fdb18c4db215194b147c7f04d
a7chic <fhoodii305@gmail.com>
2026-06-27 21:02:29 +0300

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+export default function Header(){
+
+return(
+
+<header
+style={{
+height:"70px",
+background:"#111",
+borderBottom:"1px solid #d4af37",
+display:"flex",
+alignItems:"center",
+justifyContent:"space-between",
+padding:"0 25px",
+color:"#d4af37",
+fontWeight:"bold",
+fontSize:"20px"
+}}
+>
+
+<span>👑 أناقة CHIC</span>
+
+<span>لوحة النظام الملكية</span>
+
+</header>
+
+);
+
+}
