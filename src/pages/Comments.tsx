ce194d3b34c99426b41e3814b3c7fb315679b03c
a7chic <fhoodii305@gmail.com>
2026-06-27 23:23:31 +0300

    Create Comments.tsx

---
+++ src/pages/Comments.tsx
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+
+export default function Comments(){
+
+return(
+
+<Layout title="إدارة التعليقات">
+
+<h2
+style={{
+color:"#D4AF37"
+}}
+>
+
+💬 التعليقات
+
+</h2>
+
+</Layout>
+
+);
+
+}
