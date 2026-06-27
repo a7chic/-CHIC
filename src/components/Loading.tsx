2cfdffaecc752dbce7491d0ea1b6aa5de5166274
a7chic <fhoodii305@gmail.com>
2026-06-27 22:56:32 +0300

    Create Loading.tsx

---
+++ src/components/Loading.tsx
@@ -0,0 +1,31 @@
+export default function Loading(){
+
+return(
+
+<div
+
+style={{
+
+width:"100%",
+
+height:"100%",
+
+display:"flex",
+
+justifyContent:"center",
+
+alignItems:"center",
+
+fontSize:35
+
+}}
+
+>
+
+⌛ جاري التحميل...
+
+</div>
+
+);
+
+}
