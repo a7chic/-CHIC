d297822a806367962b56c44f342a76d6ad03cd4f
a7chic <fhoodii305@gmail.com>
2026-06-28 00:46:21 +0300

    Create Notifications.tsx

---
+++ src/pages/Notifications.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+export default function Notifications() {
+
+return (
+
+<div
+style={{
+color:"#fff"
+}}
+>
+
+
+<div
+style={{
+background:"#111",
+border:"1px solid #D4AF37",
+borderRadius:"20px",
+padding:"30px",
+marginBottom:"25px"
+}}
+>
+
+
+<h1
+style={{
+color:"#D4AF37"
+}}
+>
+
+🔔 الإشعارات
+
+</h1>
+
+
+<p
+style={{
+color:"#ccc",
+fontSize:"18px"
+}}
+>
+
+تابع آخر التحديثات والعروض الخاصة بك
+
+</p>
+
+
+</div>
+
+
+
+<div
+style={{
+background:"#111",
+border:"1px solid #D4AF37",
+borderRadius:"18px",
+padding:"40px",
+textAlign:"center"
+}}
+>
+
+
+<div
+style={{
+fontSize:"60px"
+}}
+>
+
+🔔
+
+</div>
+
+
+<h2
+style={{
+color:"#D4AF37"
+}}
+>
+
+لا توجد إشعارات جديدة
+
+</h2>
+
+
+<p
+style={{
+color:"#aaa"
+}}
+>
+
+ستظهر هنا التنبيهات الخاصة بالعروض والطلبات والتحديثات
+
+</p>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
