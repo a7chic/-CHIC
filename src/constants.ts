2358dbd83155e7a959121d2df755c71875973bc2
a7chic <fhoodii305@gmail.com>
2026-06-27 22:50:31 +0300

    Create constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+export const SITE_NAME="أناقة CHIC";
+
+export const OWNER_NAME="فهد بن حمود بن فهد الشمري";
+
+export const OWNER_EMAIL="";
+
+export const OWNER_PHONE="0536667222";
+
+export const OWNER_WHATSAPP="+966507882771";
+
+export const SNAP="pmp.u";
+
+export const SITE_YEAR=2010;
+
+export const COPYRIGHT=2026;
+
+export const ROYAL_COLOR="#D4AF37";
+
+export const DARK="#050505";
