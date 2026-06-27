b2089e3462f1f93729e89ac4a7d0c4daad2edd64
a7chic <fhoodii305@gmail.com>
2026-06-28 01:26:10 +0300

    Create Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+
+export default function Footer(){
+
+return(
+
+<footer
+style={{
+height:"70px",
+background:"#111",
+borderTop:"2px solid #D4AF37",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+color:"#D4AF37",
+fontWeight:"bold",
+marginTop:"30px"
+}}
+>
+
+© 2026 ANAQA CHIC
+
+</footer>
+
+);
+
+}
