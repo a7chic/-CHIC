c99e0e6b13dae49fce4b9a7e2c3dc2e800d7f0f2
a7chic <fhoodii305@gmail.com>
2026-06-28 01:17:08 +0300

    Create Loader.tsx

---
+++ src/components/Loader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+
+export default function Loader(){
+
+return(
+
+<div
+style={{
+minHeight:"200px",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+background:"#050505",
+color:"#D4AF37",
+fontSize:"28px",
+fontWeight:"bold"
+}}
+>
+
+⏳ جاري التحميل...
+
+</div>
+
+);
+
+}
