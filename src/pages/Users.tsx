8c1dbd00355d89bf914660a7557928d2ea04c86b
a7chic <fhoodii305@gmail.com>
2026-06-27 23:22:55 +0300

    Create Users.tsx

---
+++ src/pages/Users.tsx
@@ -0,0 +1,16 @@
+import Layout from "../components/Layout";
+import Statistics from "../components/Statistics";
+
+export default function Users(){
+
+return(
+
+<Layout title="إدارة المستخدمين">
+
+<Statistics/>
+
+</Layout>
+
+);
+
+}
