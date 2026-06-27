8a45e7ae153deb3d7067a4b33313ef4558de5afc
a7chic <fhoodii305@gmail.com>
2026-06-28 00:45:35 +0300

    Create Favorites.tsx

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,103 @@
+import React from "react";
+
+export default function Favorites() {
+
+return (
+
+<div
+style={{
+color:"#fff"
+}}
+>
+
+
+<div
+style={{
+background:"#111",
+border:"1px solid #D4AF37",
+borderRadius:"20px",
+padding:"30px",
+marginBottom:"25px"
+}}
+>
+
+
+<h1
+style={{
+color:"#D4AF37"
+}}
+>
+
+❤️ المفضلة
+
+</h1>
+
+
+<p
+style={{
+color:"#ccc",
+fontSize:"18px"
+}}
+>
+
+المنتجات والإعلانات التي قمت بحفظها ستظهر هنا
+
+</p>
+
+
+</div>
+
+
+
+<div
+style={{
+background:"#111",
+border:"1px solid #D4AF37",
+borderRadius:"18px",
+padding:"40px",
+textAlign:"center"
+}}
+>
+
+
+<div
+style={{
+fontSize:"60px"
+}}
+>
+
+❤️
+
+</div>
+
+
+<h2
+style={{
+color:"#D4AF37"
+}}
+>
+
+لا توجد عناصر محفوظة حالياً
+
+</h2>
+
+
+<p
+style={{
+color:"#aaa"
+}}
+>
+
+ابدأ بإضافة المنتجات المميزة إلى قائمة المفضلة
+
+</p>
+
+
+</div>
+
+
+</div>
+
+);
+
+}
