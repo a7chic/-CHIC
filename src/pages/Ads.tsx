4d7bbed5f5be4768d7a6cc0d0ab1fb74c9350b94
a7chic <fhoodii305@gmail.com>
2026-06-27 23:23:58 +0300

    Create Ads.tsx

---
+++ src/pages/Ads.tsx
@@ -0,0 +1,23 @@
+import Layout from "../components/Layout";
+
+export default function Ads(){
+
+return(
+
+<Layout title="إدارة الإعلانات">
+
+<h2
+style={{
+color:"#D4AF37"
+}}
+>
+
+📢 جميع الإعلانات
+
+</h2>
+
+</Layout>
+
+);
+
+}
