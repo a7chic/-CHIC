ac0fb7431079f27bdbd44a87ba07e7f69e07d9ff
a7chic <fhoodii305@gmail.com>
2026-06-27 23:24:29 +0300

    Create Settings.tsx

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,22 @@
+import Layout from "../components/Layout";
+
+export default function Settings(){
+
+return(
+
+<Layout title="إعدادات الموقع">
+
+<h2
+style={{
+color:"#D4AF37"
+}}
+>
+
+⚙️ إعدادات النظام
+
+</h2>
+
+</Layout>
+
+);
+}
