3fdb85253c42a3e72f2a2b9116cc6b965114b8f2
a7chic <fhoodii305@gmail.com>
2026-06-28 01:42:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,7 @@ Route,
 Navigate
 } from "react-router-dom";
 
-
 import MainLayout from "./layouts/MainLayout";
-
 
 import Home from "./pages/Home";
 import Haraj from "./pages/Haraj";
@@ -20,10 +18,17 @@ import Profile from "./pages/Profile";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
+import Search from "./pages/Search";
+import Categories from "./pages/Categories";
+import ProductDetails from "./pages/ProductDetails";
+import AddProduct from "./pages/AddProduct";
+import Chat from "./pages/Chat";
+import AdminDashboard from "./pages/AdminDashboard";
 
+import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedAdminRoute from "./components/ProtectedAdminRoute";
 
 export default function App(){
-
 
 return(
 
@@ -31,91 +36,61 @@ return(
 
 <Routes>
 
-
 <Route
 path="/"
-element={
-<Navigate to="/login"/>
-}
+element={<Navigate to="/login"/>}
 />
-
-
 
 <Route
 path="/login"
-element={
-<Login/>
-}
+element={<Login/>}
 />
-
-
 
 <Route
 path="/register"
-element={
-<Register/>
-}
+element={<Register/>}
 />
-
-
 
 <Route
 element={
+<ProtectedRoute>
 <MainLayout/>
+</ProtectedRoute>
 }
 >
 
+<Route path="/home" element={<Home/>}/>
+
+<Route path="/haraj" element={<Haraj/>}/>
+
+<Route path="/catalog" element={<Catalog/>}/>
+
+<Route path="/favorites" element={<Favorites/>}/>
+
+<Route path="/notifications" element={<Notifications/>}/>
+
+<Route path="/profile" element={<Profile/>}/>
+
+<Route path="/search" element={<Search/>}/>
+
+<Route path="/categories" element={<Categories/>}/>
+
+<Route path="/product/:id" element={<ProductDetails/>}/>
+
+<Route path="/add-product" element={<AddProduct/>}/>
+
+<Route path="/chat/:id" element={<Chat/>}/>
 
 <Route
-path="/home"
+path="/admin"
 element={
-<Home/>
+<ProtectedAdminRoute>
+<AdminDashboard/>
+</ProtectedAdminRoute>
 }
 />
-
-
-<Route
-path="/haraj"
-element={
-<Haraj/>
-}
-/>
-
-
-<Route
-path="/catalog"
-element={
-<Catalog/>
-}
-/>
-
-
-<Route
-path="/favorites"
-element={
-<Favorites/>
-}
-/>
-
-
-<Route
-path="/notifications"
-element={
-<Notifications/>
-}
-/>
-
-
-<Route
-path="/profile"
-element={
-<Profile/>
-}
-/>
-
 
 </Route>
-
 
 </Routes>
 
