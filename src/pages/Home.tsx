44b3adadf9a0ad310f5e8b5423c4759950212c17
a7chic <fhoodii305@gmail.com>
2026-06-27 21:06:14 +0300

    Create Home.tsx

---
+++ src/pages/Home.tsx
@@ -0,0 +1,26 @@
+export default function Home(){
+
+return(
+
+<div
+style={{
+height:"100%",
+display:"flex",
+justifyContent:"center",
+alignItems:"center",
+background:"#050505",
+border:"1px solid #d4af37",
+borderRadius:"20px",
+color:"white",
+fontSize:"30px",
+fontWeight:"bold"
+}}
+>
+
+🏆 الصفحة الرئيسية الملكية
+
+</div>
+
+);
+
+}
