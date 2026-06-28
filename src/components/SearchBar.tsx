9b8360add453d1a536c9360a5804d0928210e1dc
a7chic <fhoodii305@gmail.com>
2026-06-28 18:08:25 +0300

    Update SearchBar.tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,37 +1,57 @@
 import React from "react";
 
-
-interface SearchBarProps{
+type Props={
 
 value:string;
 
 onChange:(value:string)=>void;
 
-}
+placeholder?:string;
 
-
+};
 
 export default function SearchBar({
-value,
-onChange
-}:SearchBarProps){
 
+value,
+
+onChange,
+
+placeholder="ابحث عن منتج..."
+
+}:Props){
 
 return(
 
 <input
+
 value={value}
+
 onChange={(e)=>onChange(e.target.value)}
-placeholder="🔎 ابحث عن منتج..."
+
+placeholder={placeholder}
+
 style={{
+
 width:"100%",
+
 padding:"15px",
+
 borderRadius:"12px",
-border:"1px solid #D4AF37",
+
 background:"#111",
+
+border:"1px solid #D4AF37",
+
 color:"#fff",
-fontSize:"16px"
+
+fontSize:"16px",
+
+outline:"none",
+
+boxSizing:"border-box"
+
 }}
+
 />
 
 );
