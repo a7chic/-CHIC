8c3c2704341fbc8f64e59491f6c562860bc59c10
a7chic <fhoodii305@gmail.com>
2026-06-28 18:38:08 +0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Footer(){
-
-const year=new Date().getFullYear();
 
 return(
 
 <footer
 style={{
-marginTop:"40px",
-background:"#111",
-borderTop:"2px solid #D4AF37",
+marginTop:"50px",
 padding:"25px",
-color:"#fff"
+textAlign:"center",
+background:"#111",
+borderTop:"1px solid #D4AF37",
+color:"#888"
 }}
 >
-
-<div
-style={{
-display:"flex",
-justifyContent:"space-between",
-alignItems:"center",
-flexWrap:"wrap",
-gap:"20px",
-maxWidth:"1400px",
-margin:"0 auto"
-}}
->
-
-<div>
 
 <h3
 style={{
-margin:"0 0 10px",
-color:"#D4AF37"
+color:"#D4AF37",
+marginBottom:"10px"
 }}
 >
 
@@ -42,92 +26,22 @@ color:"#D4AF37"
 
 </h3>
 
-<p
-style={{
-margin:0,
-color:"#aaa"
-}}
->
+<p>
 
-منصة فاخرة لبيع وشراء الأزياء والإكسسوارات.
+منصة الأزياء والإعلانات الفاخرة
 
 </p>
 
-</div>
-
-<div
+<p
 style={{
-display:"flex",
-gap:"18px",
-flexWrap:"wrap"
+marginTop:"15px",
+fontSize:"14px"
 }}
 >
 
-<Link
-to="/home"
-style={{
-color:"#D4AF37",
-textDecoration:"none"
-}}
->
+© {new Date().getFullYear()} ANAQA CHIC - جميع الحقوق محفوظة
 
-الرئيسية
-
-</Link>
-
-<Link
-to="/haraj"
-style={{
-color:"#D4AF37",
-textDecoration:"none"
-}}
->
-
-الحراج
-
-</Link>
-
-<Link
-to="/catalog"
-style={{
-color:"#D4AF37",
-textDecoration:"none"
-}}
->
-
-الكتالوج
-
-</Link>
-
-<Link
-to="/profile"
-style={{
-color:"#D4AF37",
-textDecoration:"none"
-}}
->
-
-حسابي
-
-</Link>
-
-</div>
-
-</div>
-
-<div
-style={{
-marginTop:"25px",
-paddingTop:"20px",
-borderTop:"1px solid #333",
-textAlign:"center",
-color:"#888"
-}}
->
-
-© {year} ANAQA CHIC. جميع الحقوق محفوظة.
-
-</div>
+</p>
 
 </footer>
 
