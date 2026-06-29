88a6264b53deb482ba060b23a204cb383b779218
a7chic <fhoodii305@gmail.com>
2026-06-30 01:21:22 +0300

    Update AddProduct.tsx

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,16 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import { auth } from "../firebase/config";
 import { addProduct } from "../services/productService";
 import ImageUpload from "../components/ImageUpload";
 import InputField from "../components/InputField";
 import Button from "../components/Button";
-// تم تعديل هذا السطر بإضافة نقطتين ليرجع خطوة للخلف ويصل لمجلد hooks بشكل صحيح
-import useAuth from "../hooks/useAuth";
 
 export default function AddProduct() {
   const navigate = useNavigate();
-  const { user } = useAuth();
+  const user = auth.currentUser;
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -81,15 +80,10 @@ export default function AddProduct() {
         </h1>
 
         <InputField label="اسم المنتج" value={title} onChange={setTitle} />
-
         <InputField label="السعر (ريال)" value={price} onChange={setPrice} />
-
         <InputField label="التصنيف" value={category} onChange={setCategory} />
-
         <InputField label="المدينة" value={city} onChange={setCity} />
-
         <InputField label="الماركة" value={brand} onChange={setBrand} />
-
         <InputField label="حالة المنتج" value={condition} onChange={setCondition} />
 
         <label
@@ -127,11 +121,7 @@ export default function AddProduct() {
             marginBottom: "20px",
           }}
         >
-          <h3
-            style={{
-              color: "#D4AF37",
-            }}
-          >
+          <h3 style={{ color: "#D4AF37" }}>
             📸 صورة المنتج
           </h3>
 
